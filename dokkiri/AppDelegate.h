95f96ba51a33961cdf1063772e79ccda554b97ab
hirokazu <iida@iida-no-macbook-pro.local>
2016-05-14 10:07:04 +0900

    Initial Commit

---
+++ dokkiri/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dokkiri
+//
+//  Created by hirokazu on 2016/05/14.
+//  Copyright © 2016年 hirokazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dokkiri/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dokkiri
+//
+//  Created by hirokazu on 2016/05/14.
+//  Copyright © 2016年 hirokazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
